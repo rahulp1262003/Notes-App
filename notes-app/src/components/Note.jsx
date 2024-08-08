f39e3f57f6a5675b4755c232979989ed494d112e
rahulp1262003 <prajapatirahul1262003@gmail.com>
2024-08-08 21:57:15 +0530

    update the title feild

--- notes-app/src/components/Note.jsx
+++ notes-app/src/components/Note.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 // import { motion } from "framer-motion"
 
 function Note({ formData }) {
+  let fd = formData.title;
+  let len = formData.title.length;
   return (
     <div className='w-[15rem] h-[15.6rem] bg-white rounded-[2.5rem] m-8 cursor-pointer overflow-hidden flex justify-between items-center flex-col'>
       <div className="w-full h-[24%] bg-blue-400 flex justify-center items-center p-3">
-        <h3 className='text-black text-[1.4rem] inline-block'>{formData.title}</h3>
+        <h3 className='text-black text-[1.4rem] inline-block'>{
+          len >= 10 ? (fd.slice(0,15) + '...') : fd
+        }</h3>
       </div>
       <div className='w-full h-[75%] inline-block justify-start items-start p-4'>
         {formData.details}
