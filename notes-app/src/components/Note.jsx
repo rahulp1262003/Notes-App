aa0cb491683ab401a82dde37a811e367a42800cb
rahulp1262003 <prajapatirahul1262003@gmail.com>
2024-08-21 00:47:59 +0530

    update the description length

--- notes-app/src/components/Note.jsx
+++ notes-app/src/components/Note.jsx
@@ -2,17 +2,19 @@ import React from 'react'
 // import { motion } from "framer-motion"
 
 function Note({ formData }) {
-  let fd = formData.title;
-  let len = formData.title.length;
+  let fd = formData.title.trim();
+  let len = fd.length;
+  let des1 = formData.details.trim();
+  let des2 = des1.length;
   return (
     <div className='w-[15rem] h-[15.6rem] bg-white rounded-[2.5rem] m-8 cursor-pointer overflow-hidden flex justify-between items-center flex-col'>
       <div className="w-full h-[24%] bg-blue-400 flex justify-center items-center p-3">
         <h3 className='text-black text-[1.4rem] inline-block'>{
-          len >= 10 ? (fd.slice(0,15) + '...') : fd
+          len >= 10 ? (fd.slice(0, 10) + '...') : fd
         }</h3>
       </div>
-      <div className='w-full h-[75%] inline-block justify-start items-start p-4'>
-        {formData.details}
+      <div className='w-full h-[75%] inline-block justify-start items-start flex-wrap p-4'>
+        <p className='text-base md:text-lg lg:text-xl text-start break-words'>{des2 >= 99 ? (des1.slice(0, 95) + '...') : des1}</p>
       </div>
     </div>
   )
